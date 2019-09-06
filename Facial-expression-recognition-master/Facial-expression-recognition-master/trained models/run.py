f3ed65fc386ff455533c5b570e0c311a12d6813e
Nguyen Minh Nhut <nmnhut@apcs.vn>
2019-09-06 10:20:45 +0700

    Update run.py

--- Facial-expression-recognition-master/Facial-expression-recognition-master/trained models/run.py
+++ Facial-expression-recognition-master/Facial-expression-recognition-master/trained models/run.py
@@ -6,64 +6,112 @@ from keras.models import Sequential
 from keras.layers import Dense, Conv2D, MaxPooling2D, Dropout, Flatten
 from keras.utils import to_categorical
 from keras.models import load_model
-pretrained_model = load_model("fer2013_mini_XCEPTION.99-0.65.hdf5")
 
 import cv2
 import matplotlib.pyplot as plt
+
+import socket; 
+import sys; 
+
+pretrained_model = load_model("fer2013_mini_XCEPTION.99-0.65.hdf5")
+faces = []
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
 
 def get_label(argument):
     labels = {0:'Angry', 1:'Disgust', 2:'Fear', 3:'Happy', 4:'Sad' , 5:'Surprise', 6:'Neutral'}
     return(labels.get(argument, "Invalid emotion"))
 
-faces = []
 
 
 def expression_recognize(img):
     faces = face_cascade.detectMultiScale(np.asarray(img), 1.3, 5)
     for (x, y, w, h) in faces:
-      if len(faces) == 1: #Use simple check if one face is detected, or multiple (measurement error unless multiple persons on image)
-        crop_img = img[y:y+h, x:x+w]
-      else:
-        print("multiple faces detected, passing over image")
+        if len(faces) == 1: #Use simple check if one face is detected, or multiple (measurement error unless multiple persons on image)
+            crop_img = img[y:y+h, x:x+w]
+        else:
+            print("multiple faces detected, passing over image")
 
+        #Resizing image to required size
+        test_image = cv2.resize(crop_img,(64,64),0,0,interpolation = cv2.INTER_AREA)
 
-    #Resizing image to required size
-    test_image = cv2.resize(crop_img,(64,64),0,0,interpolation = cv2.INTER_AREA)
+        #Converting image to array
+        test_image = np.array(test_image)
 
-    #Converting image to array
-    test_image = np.array(test_image)
+        #converting to grayscale
+        gray = cv2.cvtColor(test_image, cv2.COLOR_BGR2GRAY)
 
-    #converting to grayscale
-    gray = cv2.cvtColor(test_image, cv2.COLOR_BGR2GRAY)
+        #scale pixels values to lie between 0 and 1 because we did same to our train and test set
+        gray = gray/255
 
-    #scale pixels values to lie between 0 and 1 because we did same to our train and test set
-    gray = gray/255
+        #reshaping image (-1 is used to automatically fit an integer at it's place to match dimension of original image)
+        gray = gray.reshape(-1, 64, 64, 1)
 
-    #reshaping image (-1 is used to automatically fit an integer at it's place to match dimension of original image)
-    gray = gray.reshape(-1, 64, 64, 1)
+        res = pretrained_model.predict(gray)
 
-    res = pretrained_model.predict(gray)
+        #argmax returns index of max value
+        result_num = np.argmax(res[0])
 
-    #argmax returns index of max value
-    result_num = np.argmax(res[0])
-
-    # print predictions
-    print("\nProbabilities are " + str(res[0])+"\n")
-    print("Emotion is "+ get_label(result_num))
+        # print predictions
+        print("\nProbabilities are " + str(res[0])+"\n")
+        print("Emotion is "+ get_label(result_num))
     
-cap = cv2.VideoCapture(0); 
 
-while(cap.isOpened()):
+def start_server(): 
+  
+
+
+  HOST = '' # Symbolic name meaning all available interfaces
+  PORT = 8888 # Arbitrary non-privileged port
+
+  s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  print ('Socket created')
+
+  #Bind socket to local host and port
+  try:
+    s.bind((HOST, PORT))
+  except socket.error as msg:
+    print ('Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1])
+    sys.exit()
+  
+  print ('Socket bind complete')
+
+  #Start listening on socket
+  s.listen(10)
+  print ('Socket now listening')
+
+
+
+  #now keep talking with the client
+  while 1:
+    #wait to accept a connection - blocking call
+    conn, addr = s.accept()
+    print ('Connected with ' + addr[0] + ':' + str(addr[1]))
+    # send a thank you message to the client.  
+    
+    conn.send('Thank you for connecting'.encode('utf-8')) 
+
+  s.close()
+
+def video_process(): 
+  cap = cv2.VideoCapture("C:/Users/nmnhut/Pictures/Camera Roll/test.mp4")
+  while(cap.isOpened()):
     ret, frame = cap.read()
-    cv2.imshow('frame',frame)
-    expression_recognize(frame); 
+    if (ret):
+        #cv2.imshow('frame',frame)
+        expression_recognize(frame);
+    else:
+        break
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
+    if (not ret):
+        break
 
-cap.release()
-cv2.destroyAllWindows()
+  cap.release()
+  cv2.destroyAllWindows()
 
 
-img = cv2.imread("surprise.jpg")
+video_process();
+start_server();
 
 

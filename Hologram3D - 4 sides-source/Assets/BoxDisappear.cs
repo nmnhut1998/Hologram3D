fa4be1d1e612cdf4a375c50edfe5bec1d38d104e
Nguyen Minh Nhut <nmnhut@apcs.vn>
2019-09-07 16:52:07 +0700

    .

---
+++ Hologram3D - 4 sides-source/Assets/BoxDisappear.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxDisappear : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private int frameCount; 
+    void Start()
+    {
+        frameCount = 0;         
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        frameCount += 1; 
+        if (frameCount == 180)
+        {
+            this.gameObject.SetActive(false); 
+        }
+    }
+}
